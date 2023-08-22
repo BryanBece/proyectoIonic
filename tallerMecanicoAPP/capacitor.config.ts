b23180f8fcd34ab2ebe783e842e8c174caa7a5f2
bryanbece <br.becerra.p@gmail.com>
2023-08-22 19:59:37 -0400

    Creacion APP

---
+++ tallerMecanicoAPP/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tallerMecanicoAPP',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;

408075a4ba3b5cca573c8c400bef0e9c03dfdaf1
bryanbece <br.becerra.p@gmail.com>
2023-08-30 12:40:10 -0400

    App 1.1

--- tallerMecanicoAPP/src/app/pages/forgot-password/forgot-password.page.ts
+++ tallerMecanicoAPP/src/app/pages/forgot-password/forgot-password.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
 })
-export class ForgotPasswordPage implements OnInit {
+export class ForgotPasswordPage {
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(public alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Envio Exitoso',
+      message: '¡Revisa tu correo!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
-
 }

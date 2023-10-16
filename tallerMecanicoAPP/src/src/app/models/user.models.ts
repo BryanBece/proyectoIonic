efdae3b162861a0a8b73fe6348876dd6c16f6c35
Bryanbece <br.becerra.p@gmail.com>
2023-10-16 00:01:12 -0300

    16-10

--- tallerMecanicoAPP/src/src/app/models/user.models.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    uid: string;
-    email: string;
-    password: string;
-    name: string;
-    telefono: string;
-    perfil: 'cliente' | 'mecanico' | 'admin' | 'administrativo'
-}

df66bb320d69314fd4596e305778cf13fa6371ba
Bryanbece <br.becerra.p@gmail.com>
2023-10-18 13:38:38 -0300

    18-10 Productos OK

--- tallerMecanicoAPP/src/app/shared/component/add-update-products/add-update-products.component.ts
+++ tallerMecanicoAPP/src/app/shared/component/add-update-products/add-update-products.component.ts
@@ -19,14 +19,18 @@ export class AddUpdateProductsComponent  implements OnInit {
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     description: new FormControl('', [Validators.required, Validators.minLength(3)]),
     price: new FormControl('', [Validators.required, Validators.min(0)]),
-    stock: new FormControl('', [Validators.required, Validators.min(0)]),
     image: new FormControl('', [Validators.required]),
     soldUnits: new FormControl('', [Validators.required, Validators.min(0)]),
     // Añadir mas campos
   })
 
-  ngOnInit() { }
+  
 
+  ngOnInit() { 
+    this.utilsSvc.getFromLocalStorage('user');
+  }
+
+  user = this.utilsSvc.getFromLocalStorage('user');
   // Tomar/Seleccionar foto
 
   async takePicture() {
@@ -40,14 +44,35 @@ export class AddUpdateProductsComponent  implements OnInit {
 
     if (this.form.valid) {
 
+      let path = 'users/' + this.user.uid + '/products';
+
       const loading = await this.utilsSvc.loading();
       await loading.present();
 
-      this.firebaseSvc.signUp(this.form.value as User).then(async res => {
+      // Subir imagen y obtener url
 
-        await this.firebaseSvc.updateUser(this.form.value.name)
-        let uid = res.user.uid;
+      let dataUrl = this.form.value.image;
+      let imagePath = this.user.uid+`/${Date.now()}`;
+      let imageUrl = await this.firebaseSvc.uploadImage(imagePath, dataUrl);
+      this.form.controls.image.setValue(imageUrl);
 
+      delete this.form.value.id;
+
+      this.firebaseSvc.addDocument(path, this.form.value).then(async res => {
+
+        this.utilsSvc.dismissModal( { success: true });
+        console.log(path);
+        console.log(imageUrl);
+        console.log(this.user);
+        console.log(`${this.user.uid}`);
+
+        this.utilsSvc.presentToast({
+          message: 'Producto creado correctamente',
+          duration: 2500,
+          color: 'success',
+          position: 'middle',
+          icon: 'checkmark-circle-outline'
+        })
 
       }).catch(error => {
         console.log(error);

abd3a97211f700dbfe9ae035649d9693f4280f8b
Bryanbece <br.becerra.p@gmail.com>
2023-11-28 22:59:02 -0300

    Add AngularFireModule and necessary imports to component specs

--- tallerMecanicoAPP/src/app/shared/component/attentions/attentions.component.spec.ts
+++ tallerMecanicoAPP/src/app/shared/component/attentions/attentions.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import firebase from 'firebase/compat/app';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { AttentionsComponent } from './attentions.component';
 
 describe('AttentionsComponent', () => {
@@ -10,13 +13,23 @@ describe('AttentionsComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AttentionsComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        // Other necessary imports
+      ],
+      providers: [
+        // Other necessary providers
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(AttentionsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- tallerMecanicoAPP/src/app/shared/component/take-hours/take-hours.component.spec.ts
+++ tallerMecanicoAPP/src/app/shared/component/take-hours/take-hours.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import firebase from 'firebase/compat/app';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TakeHoursComponent } from './take-hours.component';
 
 describe('TakeHoursComponent', () => {
@@ -10,13 +13,23 @@ describe('TakeHoursComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TakeHoursComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        // Other necessary imports
+      ],
+      providers: [
+        // Other necessary providers
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(TakeHoursComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();

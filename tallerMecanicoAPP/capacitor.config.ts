34b3a7fe6e7f2531a20710c1df3c49c2b73d8339
Bryanbece <br.becerra.p@gmail.com>
2023-11-29 19:29:33 -0300

    Update app name and version

--- tallerMecanicoAPP/capacitor.config.ts
+++ tallerMecanicoAPP/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Triumviratus',
+  appName: 'Triumviratus APP',
   webDir: 'www',
   server: {
     androidScheme: 'https'
